866d2f8e46aeafd435e395d662e873246b675b66
honda <g031u143@s.iwate-pu.ac.jp>
2024-12-05 17:25:37 +0900

    sound_python from Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.chaquo.python")
 }
 
 android {
     namespace = "com.example.team4application"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.team4application"
@@ -15,9 +16,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
+
+        ndk {
+            // On Apple silicon, you can omit x86_64.
+            abiFilters += listOf("arm64-v8a", "x86_64")
         }
+
     }
 
     buildTypes {
@@ -29,6 +33,8 @@ android {
             )
         }
     }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,15 +42,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+}
+
+chaquopy {
+    defaultConfig {
+        buildPython("/opt/anaconda3/bin/python3")
+
+        pip {
+            // numpyをインストールする
+            install("numpy")
+            // pandasをインストールする
+            install("pandas")
+            /* requirements.txtを指定することも可能
+            install("-r", "requirements.txt")
+            */
         }
     }
 }
@@ -52,13 +63,6 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
@@ -66,8 +70,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/example/team4application/MainActivity.kt
+++ app/src/main/java/com/example/team4application/MainActivity.kt
@@ -1,42 +1,45 @@
 package com.example.team4application
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
-import android.view.View
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.chaquo.python.Python
+import com.chaquo.python.android.AndroidPlatform
+import android.widget.TextView
+import android.widget.Toast
+import com.chaquo.python.PyException
 
-class MainActivity : ComponentActivity() {
-    private lateinit var vibrator: Vibrator
 
-    private val timings = longArrayOf(0, 100, 100, 100, 100, 100, 100, 100)
-    private val amplitudes = intArrayOf(0, 255, 0, 255, 0, 255, 0, 255)
-    private val repeatIndex = -1
-
+class MainActivity : AppCompatActivity() {
+    //    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        vibrator = getSystemService(Vibrator::class.java)
-    }
+        // Pythonコードを実行する前にPython.start()の呼び出しが必要
+        if (!Python.isStarted()) {
+            Python.start(AndroidPlatform(this))
+        }
 
-    fun moveToMenu(view: View) {
-        val intent = Intent(this@MainActivity, VibrateMenuActivity::class.java)
-        startActivity(intent)
-    }
+        val py = Python.getInstance()
+        val module = py.getModule("hello") // スクリプト名
 
-    // Vibrate the device
-    fun vibrate(view: View) {
-        if (::vibrator.isInitialized && vibrator.hasVibrator()) {
-            vibrator.vibrate(VibrationEffect.createWaveform(timings, amplitudes, repeatIndex))
+        try {
+            val randomNumber = module.callAttr("create_random_number").toFloat() // 関数名
+            findViewById<TextView>(R.id.text_view).text = randomNumber.toString()
+        } catch (e: PyException) {
+            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
         }
     }
-
-    // turn on the microphone
-    fun onMicrophoneClick(view: View) {
-    }
 }
-
